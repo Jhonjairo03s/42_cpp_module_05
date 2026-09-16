036a8c3d28476c21e0c44a58f7f361271d5f205a
Jhon Valencia <jhonjairo.1331@gmail.com>
2026-09-16 19:57:20 +0200

    Realización de las declaraciones

---
+++ Bureaucrat.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhvalenc <jhvalenc@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/16 12:23:07 by jhvalenc          #+#    #+#             */
+/*   Updated: 2026/09/16 19:50:13 by jhvalenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef  BUREAUCRAT_HPP
+# define    BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+
+class   Bureaucrat
+{
+    private:
+        const std::string   _name;
+        int                 _grade;
+    public:
+        // Forma Canónica Ortodoxa
+        Bureaucrat();
+        Bureaucrat(const Bureaucrat& other);
+        Bureaucrat& operator=(const Bureaucrat& other);
+        ~Bureaucrat();
+        // Constructor parametrizado
+        Bureaucrat(const std::string name, int grade);
+        //Herencia exception
+        class   GradeTooHighException : public std::exception
+        {
+            private:
+                std::string _msgHigh;
+            public:
+                GradeTooHighException(const std::string& msg);
+                virtual const char* what() const throw();
+        };
+        class   GradeTooLowException : public std::exception
+        {
+            private:
+                std::string _msgLow;
+            public:
+                GradeTooLowException(const std::string& msg);
+                virtual const char* what() const throw();
+        };
+        // Funiones Miembro
+        void    increment();
+        void    decrement();
+        // Setter y Getter
+        int getGrade(void) const;
+        const std::string&    getName(void) const;
+};
+
+std::ostream&   operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
